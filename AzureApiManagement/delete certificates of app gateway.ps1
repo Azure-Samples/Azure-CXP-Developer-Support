a52ca0225a671993a731dd473829b889281154c8
RakeshMohan-MSFT <49954584+RakeshMohanMSFT@users.noreply.github.com>
2022-06-06 14:05:02 +0530

    Update delete certificates of app gateway.ps1

--- AzureApiManagement/delete certificates of app gateway.ps1
+++ AzureApiManagement/delete certificates of app gateway.ps1
@@ -1,4 +1,7 @@
-﻿Connect-AzureRmAccount
+//DISCLAIMER
+//The sample codes are not supported under any Microsoft standard support program or service. The sample codes are provided AS IS without warranty of any kind. Microsoft further disclaims //all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or //performance of the sample codes and documentation remains with you. In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, //production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business //information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such //damages
+
+Connect-AzureRmAccount
 $appgwName = "AppGateway-APIM"
 $resgpName = "sf-with-apim-rg"
 $gw = Get-AzureRmApplicationGateway -Name $appgwName -ResourceGroupName $resgpName
@@ -12,4 +15,3 @@ $gw = Set-AzureRmApplicationGateway -ApplicationGateway $gw
 Remove-AzureRmApplicationGatewaySslCertificate -ApplicationGateway $gw -Name "appgatewaycert"
 Remove-AzureRmApplicationGatewaySslCertificate -ApplicationGateway $gw -Name "proxycert"
 Remove-AzureRmApplicationGatewaySslCertificate -ApplicationGateway $gw -Name "proxycertwithchain"
-
