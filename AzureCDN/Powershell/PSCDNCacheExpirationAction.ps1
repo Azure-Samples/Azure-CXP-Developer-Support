839e1eefcfdac4500b408268bccbe138ac8fdde2
navba-MSFT <57353862+navba-MSFT@users.noreply.github.com>
2022-08-23 18:06:04 +0530

    Update PSCDNCacheExpirationAction.ps1

--- AzureCDN/Powershell/PSCDNCacheExpirationAction.ps1
+++ AzureCDN/Powershell/PSCDNCacheExpirationAction.ps1
@@ -1,13 +1,13 @@
-//DISCLAIMER
-//The sample script are not supported under any Microsoft standard support program or service. The sample script are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample codes and documentation remains with you. In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages 
+# DISCLAIMER
+# The sample script are not supported under any Microsoft standard support program or service. The sample script are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample codes and documentation remains with you. In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages 
 
-// Script Overview and Pre-requisites
-// This sample script uses the Az.CDN module to create a CacheExpirationAction.
-// Before running this, you need to create an Azure CDN resource within your Azure Subscription.
-// Update all the variable details in the below code before running the sample.
+# Script Overview and Pre-requisites
+# This sample script uses the Az.CDN module to create a CacheExpirationAction.
+# Before running this, you need to create an Azure CDN resource within your Azure Subscription.
+# Update all the variable details in the below code before running the sample.
 
 
-//Sample Code
+# Sample Code
 
 $ruleCacheAction=[Microsoft.Azure.PowerShell.Cmdlets.Cdn.Models.Api20210601.DeliveryRuleCacheExpirationAction]::New()
 $ruleCacheAction.Name=[Microsoft.Azure.PowerShell.Cmdlets.Cdn.Support.DeliveryRuleAction]::CacheExpiration
